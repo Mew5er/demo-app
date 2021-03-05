43e1e6989c9e7a7c08119bde9dbadf74a89bb98e
Nishant kumar Gupta <nishantg1210@gmail.com>
2021-03-05 10:49:33 +0530

    pushing some files

---
+++ myApp.js
@@ -0,0 +1,9 @@
+const app= require("express")();
+const PORT= process.env.port || 3000;
+app.get("",(req,res) => {
+    res.send("Hello World! This API is awesome");
+});
+
+app.listen(PORT,()=>{
+    console.log(`App up at port ${PORT}`);
+});
