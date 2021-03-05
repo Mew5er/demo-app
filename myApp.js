e45c3c32c930a4714e6aa8050d3ad5529c97246b
Nishant kumar Gupta <nishantg1210@gmail.com>
2021-03-05 11:32:38 +0530

    changes to js file

--- myApp.js
+++ myApp.js
@@ -1,5 +1,5 @@
 const app= require("express")();
-const PORT= process.env.port || 3000;
+const PORT= process.env.PORT || 3000;
 app.get("",(req,res) => {
     res.send("Hello World! This API is awesome");
 });
